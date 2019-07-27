484953416efee3ee633bb5b41899ed7d97931524
Chris Cardozo <cardocp@hotmail.com>
2019-07-27 08:26:39 -0400

    updates to install directions

--- Exile.Altis/config.hpp
+++ Exile.Altis/config.hpp
@@ -1,4 +1,32 @@
 
+	class Laptop
+	{
+		targetType = 2;
+		target = "Exile_Construction_Laptop_Static";
+
+		class Actions
+		{
+			class CameraSystem: ExileAbstractAction
+			{
+				title = "CCTV Access";
+				condition = "((ExileClientInteractionObject animationPhase 'LaptopLidRotation') >= 0.5)";
+				action = "_this call ExileClient_gui_baseCamera_show";
+			};
+			
+			/*
+			Be sure to add this
+			*/
+			class VirtualGarage: ExileAbstractAction
+			{
+			  title = "Access Virtual Garage";
+			  condition = "((ExileClientInteractionObject animationPhase 'LaptopLidRotation') >= 0.5)";
+			  action = "call ExileClient_VirtualGarage_AccessGarage";
+			};
+			/*
+			END 
+			*/
+		};
+	};
 
 class CfgNetworkMessages {
 	#include "VirtualGarage\Configs\NetworkMessages.hpp"	
---
+++ Exile.Altis/init.sqf
@@ -0,0 +1,3 @@
+// Merge what appears below with your current init.sqf
+
+[] execVM "addons\VirtualGarage\VirtualGarage_Client_Init.sqf";
