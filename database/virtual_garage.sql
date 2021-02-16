4dcc5e84af888460f3bcdb9c7dd6547beb9d3278
Chris Cardozo <cardocp@hotmail.com>
2021-02-16 18:03:47 -0500

    Corrected Issues with sql files.

--- database/virtual_garage.sql
+++ database/virtual_garage.sql
@@ -1,16 +1,4 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               8.0.12 - MySQL Community Server - GPL
--- Server OS:                    Win64
--- HeidiSQL Version:             9.5.0.5196
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
+-
 -- Dumping structure for table exile.virtual_garage
 CREATE TABLE IF NOT EXISTS `virtual_garage` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -28,9 +16,10 @@ CREATE TABLE IF NOT EXISTS `virtual_garage` (
   `cargo_containers` text,
   `position` text,
   `vector` text,
-  `stored_at` datetime DEFAULT NULL,
   `nickname` varchar(64) DEFAULT '',
+  `wep_loadout` text,
+  `stored_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4986 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5059 DEFAULT CHARSET=utf8;
 
 
